93b930b8ce6f9b03cd5f3bca0c0f3cff4cce8ee3
icodeunot <jsal.dev@protonmail.com>
2025-07-13 01:32:21 +0000

    Allow PA host in ALLOWED_HOSTS

--- spica/settings.py
+++ spica/settings.py
@@ -25,7 +25,8 @@ sys.path.append(str(BASE_DIR))
 # These were sent to the .env and .gitignore
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'dev-secret-key')
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['laziestj.pythonanywhere.com']
+
 
 
 
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
